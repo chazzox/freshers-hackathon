3816914720bba9eebf61279402a65bedf168bc64
Danny John Piper <djpiper28@gmail.com>
2021-09-25 14:48:44 +0100

    remove bin

--- bin/CMakeFiles/CursedRTS_Hackathon.dir/src/utils.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/CursedRTS_Hackathon.dir/src/utils.c.o: \
- /home/danny/freshers-hackathon/src/utils.c /usr/include/stdc-predef.h
--- bin/CMakeFiles/prepy.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/prepy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/prepy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
